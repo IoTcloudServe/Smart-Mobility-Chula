1a920bd7a80e13dab53be06830399abad6dd772c
Ei Ei Mon <44397503+EEM0N@users.noreply.github.com>
2021-01-15 09:18:04 +0700

    Add files via upload

---
+++ Rama-4 Model/KasemRat-SUMO(Using_cutRoutes_GridlockMasuk)/build.bat
@@ -0,0 +1,2 @@
+python "%SUMO_HOME%\tools\randomTrips.py" -n osm_new.net.xml --seed 42 --fringe-factor 5 -p 1.595977 -o osm.passenger.trips.xml -e 3600 --vehicle-class passenger --vclass passenger --prefix veh --min-distance 300 --trip-attributes "departLane=\"best\"" --fringe-start-attributes "departSpeed=\"max\"" --allow-fringe.min-length 1000 --lanes --validate --additional-file vtype.add.xml --edge-permission passenger
+   --edge-permission passenger
